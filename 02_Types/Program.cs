222863937a107ccf1b718c927e670cf0dd36cd31
mariaharrell13 <mariaharrell13@gmail.com>
2022-02-02 11:14:34 -0500

    02_Types added

---
+++ 02_Types/Program.cs
@@ -0,0 +1,14 @@
+﻿// bool is a true fals value, no quotes around the word
+// this is because true and false are keywords, not strings
+bool isLoggedIn = true;
+Console.WriteLine(isLoggedIn);
+
+// integers, or int, are whole numbers only
+int age = 42;
+Console.WriteLine(age);
+
+//decimal has a lot of possible decimal points
+// it also needs the m suffix to now it's a decimal type
+decimal accountBalance = 9000.01m;
+Console.WriteLine(accountBalance);
+//Notice that when the accountBalance gets written to the console there is no m suffix in the output
