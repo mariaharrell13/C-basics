a3a59b12c13c4cbd4c8c42cf7347bdd35ebd0bc2
mariaharrell13 <mariaharrell13@gmail.com>
2022-02-03 10:48:54 -0500

    Ternaries page added to Conditionals

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,14 @@
+﻿// Here we're using Write instead of WriteLine
+Console.Write("Enter your age: ");
+string response = System.Console.ReadLine();
+int age = int.Parse(response);
+
+// 1        // 2     // 3             // 4
+string output = age >=18 ? "you can vote!" : "You're too young to vote.";
+
+// 1 - Variable to hold returned value
+// 2 - Boolean expression to be evaluated
+// 3 - Value to be returned if 2 is true
+// 4 - Value to be returned if 2 is false
+
+Console.WriteLine(output);
