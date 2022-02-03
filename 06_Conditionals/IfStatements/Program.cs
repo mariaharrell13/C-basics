1e0187b39ca8d1826688abe3763ec6c3e2d65d33
mariaharrell13 <mariaharrell13@gmail.com>
2022-02-03 10:23:00 -0500

    Conditional Basics Added

---
+++ 06_Conditionals/IfStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿bool isSnowing = true;
+bool isGoingOutside = true;
+
+if (isSnowing)
+{
+    Console.WriteLine("It's snowing out!");
+}
+
+// Is snowing And is going outside
+if (isSnowing && isGoingOutside)
+{
+    Console.WriteLine("Better bring and umbrella!");
+}
+
+// Not raining OR not going outside
+if (!isSnowing || !isGoingOutside)
+{
+    Console.WriteLine("I do not need an umbrella.");
+}
+
+// Exclusively not raining OR going outside
+if (isSnowing ^ isGoingOutside)
+{
+    Console.WriteLine("I will only go outside if it is not snowing.");
+}
