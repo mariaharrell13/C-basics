7dfcf00a1e803053deb3f24f0def13c16abeb73d
mariaharrell13 <mariaharrell13@gmail.com>
2022-02-07 10:01:49 -0500

    While added to Loops Fundamentals

---
+++ 07_Loops/While/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declaring a variable to use in out loop's boolean expression
+int number = 0;
+
+//While the value in number is less than or equal to 1000, run the fillowing
+while (number <= 100)
+{
+    //write the value of number to the console and then increment by 5
+    Console.WriteLine(number);
+    number = number +5;
+}
